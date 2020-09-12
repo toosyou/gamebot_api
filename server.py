6870a5789e4082f6cfc2bd5d9aff30bac7029577
toosyou <toosyou.tw@gmail.com>
2020-09-13 00:41:50 +0800

    working baba is you version

--- server.py
+++ server.py
@@ -1,13 +1,29 @@
 from flask import Flask
 from flask import request
+import configparser
+from ahk import AHK
 
 app = Flask(__name__)
 
-@app.route('/')
+config = configparser.ConfigParser()
+config.read('config.cfg')
+avaliable_controls = [name for name, value in config.items('control')]
+
+ahk = AHK()
+window = ahk.find_window(title=config['general']['title'].encode())
+
+def press(key="", window=window):
+    window.send(key, press_duration=110)
+
+@app.route('/', methods=['GET'])
 def receive_control():
     control = request.args.get('control')
-    print(control)
+
+    if control in avaliable_controls:
+        press(config['control'][control])
+
     return ''
 
 if __name__ == '__main__':
+    window.activate()
     app.run(host='0.0.0.0', port=80)
